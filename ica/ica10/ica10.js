654060e6981e68e611f9fb94b70a7025b4037ec0
rebeccakohler1 <reko2836@colorado.edu>
2025-10-23 14:15:31 -0600

    ica10

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,42 @@
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click', newTrivia);
+
+let answerBtn = document.querySelector('#js-tweet');
+addEventListener('click', newAnswer);
+
+let current = {
+    question: "",
+    answer: "",
+}
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function newTrivia() {
+   // console.log("Success");
+
+   try {
+   const response = await fetch(endpoint);
+   if (!response.ok) {
+    throw Error(response.statusText)
+   }
+   const json = await response.json();
+   //console.log(json);
+   displayTrivia(json["question"]);
+   current.question = json["question"];
+   current.answer = json["answer"];
+   } catch (err) {
+    console.log(err)
+    alert('Filed to get new trivia');
+   }
+}
+
+function displayTrivia(question) {
+    const questionText = document.querySelector('#js-quote-text');
+    const answerText = document.querySelector('#js-answer-text');
+    questionText.textContent = question;
+    answerText.textContent = "";
+}
+function newAnswer() {
+    //console.log("Success == answer!");
+    const answerText = document.querySelector("#js-answer-text");
+    answerText.textContent = current.answer;
+}
